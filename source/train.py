5ee792e41747b03b69d13d83d2064deaf18c5332
Roman Schweikert <roman.schweikert@gmail.com>
2025-02-17 14:48:31 +0100

    reverted back to TF_GPU_ALLOCATOR=cuda_malloc_async

--- source/train.py
+++ source/train.py
@@ -18,14 +18,6 @@ def train_model(model, train_dataset, eval_dataset, epochs=10, callbacks=None):
 def main():
     gpus = tf.config.list_physical_devices('GPU')
     print("Checking available GPUs:", gpus)
-    if gpus:
-        try:
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            print(f"GPU memory growth enabled for: {gpus}")
-        except RuntimeError as e:
-            print(e)
-
     params = config.Params()
     random_seed = params['general']['random_seed']
     epochs = params['train']['epochs']
